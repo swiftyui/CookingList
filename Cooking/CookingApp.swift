c4e1026d895477ddedb4da12597bcbf048166968
Arno van Zyl <vanzylarno@yahoo.com>
2022-08-23 14:06:17 +0200

    Initial Commit

---
+++ Cooking/CookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingApp.swift
+//  Cooking
+//
+//  Created by Arno van Zyl on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
